c787a7180ef223a5964175587d4bc52e3beb186d
ALI3E-XD <106314895+AlICEWORLDWIDE@users.noreply.github.com>
2022-08-05 09:55:19 +0100

    Update PJT.py

--- PJT.py
+++ PJT.py
@@ -1,1 +1,12 @@
-
+import os,time,rich
+from rich.panel import Panel as nel
+from rich import print as cetak
+ 
+__alvino__ganteng__ = '\t[yellow]•[white] [green]WELCOME[white] TO JEWEL CYBER TOOL[yellow] •[white]'
+cetak(nel(__alvino__ganteng__));time.sleep(1)
+ 
+if __name__ == "__main__":
+        try:
+                __import__("PJT").login()
+        except Exception as e:
+                exit(str(e))
